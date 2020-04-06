4cf99ddf1003611cbf6fec42056ae37c49131994
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-06 22:17:52 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,15 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  array.count
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  empty_strings = 0 
+  array.count do |index|
+    if array[index] == ""
+      empty_strings += 1 
+    end
+  end
+  empty_strings
 end
